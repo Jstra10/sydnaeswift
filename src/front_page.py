5424a5ecef10fed33841b55e9adc6a2091dad62a
Jstra10 <jjs187@gmail.com>
2023-11-06 14:17:47 -0500

    'Update

--- src/front_page.py
+++ src/front_page.py
@@ -1,8 +1,14 @@
 
 from PIL import Image
 import streamlit as st
+import pandas as pd
+import os
+from pathlib import Path
 
 st.title("Sydnaes Taylor Swift Application")
+folder_dir = os.path.join(Path(__file__).parents[0])
+counties = pd.read_csv(f'{folder_dir}\\data\\ts.png', low_memory=False)
+
 
 image_path = 'C:/Users/jjs61/OneDrive/Desktop/News/src/data/ts.png'
 image = Image.open(image_path)
